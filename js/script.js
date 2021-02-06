2ec2c87120a6e46bc88fdd877f6b56ade12a6b44
Chris Clements <chrisclementss@gmail.com>
2021-02-05 22:59:19 -0500

    api linked

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 let weatherAPIKey = '54653dca6878a4649b659650fd3cbc6a';
+let weatherBaseEndpoint = 'https://api.openweathermap.org/data/2.5/weather?units=metric&appid=' + weatherAPIKey;
+
+let getWeatherByCityName = async (city) => {
+    let endpoint = weatherBaseEndpoint + '&q=' + city;
+    let response = await fetch(endpoint);
+    let weather = await response.json();
+
+    console.log(weather);
+}
+
+getWeatherByCityName('New York');
